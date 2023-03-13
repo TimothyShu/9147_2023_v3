3a08ba58ad5c14cf551c63f26c59d0255dec7a07
TimothyShu <123728889+TimothyShu@users.noreply.github.com>
2023-03-14 07:40:17 +1100

    fixed pathing

--- src/main/java/frc/robot/SubsystemsAll.java
+++ src/main/java/frc/robot/SubsystemsAll.java
@@ -1,22 +1,23 @@
+package frc.robot;
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package Subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Subsystems.*;
 
-public class Subsystems extends SubsystemBase {
+public class SubsystemsAll extends SubsystemBase {
   /** Creates a new Subsystems. */
   public DriveSubsystem driveSubsystem;
   public ArmSubsystem armSubsystem;
   public GrabberSubsystem grabberSubsystem;
   public VisionSubsystem visionSubsystem;
   public GyroSubsystem gyroSubsystem;
-  public Subsystems() {
+  public SubsystemsAll() {
     driveSubsystem = new DriveSubsystem();
-    armSubsystem = new DriveSubsystem();
+    armSubsystem = new ArmSubsystem();
     grabberSubsystem = new GrabberSubsystem();
     visionSubsystem = new VisionSubsystem();
     gyroSubsystem = new GyroSubsystem();
