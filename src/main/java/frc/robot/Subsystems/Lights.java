87be76510fd51afe1334047b776c67344800d926
Tim <70928@joeys.org>
2023-05-15 15:49:12 +1000

    created lights subsystem

---
+++ src/main/java/frc/robot/Subsystems/Lights.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+//create the object linked to the LED lights -refer to the WPI lib docs
+//create a series of methods/functions that allows us to switch the lights on,off and choose colours
+
+public class Lights extends SubsystemBase {
+  /** Creates a new Lights. */
+  public Lights() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
