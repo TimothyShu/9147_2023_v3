494c11eba543cfc53dafbd0a2b34e5e1915d8314
Tim Shu <tims11105@gmail.com>
2023-05-16 21:47:03 +1000

    removed unused import

--- src/main/java/frc/robot/Subsystems/PneumaticGrabber.java
+++ src/main/java/frc/robot/Subsystems/PneumaticGrabber.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Variables.SubsystemVariables;
