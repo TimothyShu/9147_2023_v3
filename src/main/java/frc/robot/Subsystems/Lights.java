c3a05705094a832319707c03876d6f7edc8d192e
Tim <70928@joeys.org>
2023-05-29 15:41:55 +1000

    Lights

--- src/main/java/frc/robot/Subsystems/Lights.java
+++ src/main/java/frc/robot/Subsystems/Lights.java
@@ -4,6 +4,8 @@
 
 package frc.robot.Subsystems;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 //create the object linked to the LED lights -refer to the WPI lib docs
@@ -11,7 +13,20 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Lights extends SubsystemBase {
   /** Creates a new Lights. */
-  public Lights() {}
+  AddressableLED LED = new AddressableLED(0);
+  AddressableLEDBuffer LED_Buffer = new AddressableLEDBuffer(30);
+
+  public Lights() {
+    LED.setLength(LED_Buffer.getLength());
+    LED.setData(LED_Buffer);
+    LED.start();
+  }
+
+  public void Set_Colour(Integer Red, Integer Green, Integer Blue) {
+    for (var i = 0; i < LED_Buffer.getLength(); i++) {
+      LED_Buffer.setRGB(i, Red, Green, Blue);
+    }
+  }
 
   @Override
   public void periodic() {
